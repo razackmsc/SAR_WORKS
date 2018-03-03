ac47a57bc28306640da32b02f13a9187a7053546
razackmsc <srazackmsc@gmail.com>
2018-03-03 23:23:19 +0530

    setup1

---
+++ app/ts/main.ts
@@ -0,0 +1,7 @@
+console.log('Typescript Start....');
+
+function getName(name:string){
+    return `Hello ${name}`;
+}
+
+document.getElementById("target").innerHTML="This is from typescript--- "+getName("Abdul Razack");
---
+++ public/js/main.js
@@ -0,0 +1,5 @@
+console.log('Typescript Start....');
+function getName(name) {
+    return "Hello " + name;
+}
+document.getElementById("target").innerHTML = "This is from typescript--- " + getName("Abdul Razack");
